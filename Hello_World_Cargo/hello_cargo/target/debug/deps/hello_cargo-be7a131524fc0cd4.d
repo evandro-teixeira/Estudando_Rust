f364ce244b0bffc7e95a21fe669c67cdbda79710
evandro-teixeira <evandrolst@live.com>
2022-07-11 23:48:12 -0300

    Add o segundo projeto - Hello world - criado com o cargo

---
+++ Hello_World_Cargo/hello_cargo/target/debug/deps/hello_cargo-be7a131524fc0cd4.d
@@ -0,0 +1,5 @@
+/home/evandro/GitHub/Estudando_Rust/Hello_World_Cargo/hello_cargo/target/debug/deps/hello_cargo-be7a131524fc0cd4: src/main.rs
+
+/home/evandro/GitHub/Estudando_Rust/Hello_World_Cargo/hello_cargo/target/debug/deps/hello_cargo-be7a131524fc0cd4.d: src/main.rs
+
+src/main.rs:
---
+++ Hello_World_Cargo/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/evandro/GitHub/Estudando_Rust/Hello_World_Cargo/hello_cargo/target/debug/hello_cargo: /home/evandro/GitHub/Estudando_Rust/Hello_World_Cargo/hello_cargo/src/main.rs
